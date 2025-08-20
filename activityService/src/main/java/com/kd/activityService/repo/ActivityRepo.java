c67e3a138b6e79d3b3fc9061685b47393c8e8cba
Kalana_KD <kalanakd471@gmail.com>
2025-08-20 09:12:09 +0530

    Create ActivityRepo.java file

---
+++ activityService/src/main/java/com/kd/activityService/repo/ActivityRepo.java
@@ -0,0 +1,17 @@
+package com.kd.activityService.repo;
+
+import com.kd.activityService.dto.ActivityRequest;
+import com.kd.activityService.entity.Activity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ActivityRepo extends MongoRepository<Activity, String> {
+    @Query(value = "{ 'userId': ?0 }", collation = "{ 'locale': 'simple' }")
+    List<Activity> findByUserId(String userId);
+
+    @Query(value = "{ '_id': ?0 }", collation = "{ 'locale': 'en' }")
+    Optional<Activity> findByActivityId(String activityId);
+}
