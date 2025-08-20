b44a041405be54cd656f292973be00dff4379327
Kalana_KD <kalanakd471@gmail.com>
2025-08-20 09:11:59 +0530

    Create ActivityController.java file

---
+++ activityService/src/main/java/com/kd/activityService/controller/ActivityController.java
@@ -0,0 +1,34 @@
+package com.kd.activityService.controller;
+
+import com.kd.activityService.dto.ActivityRequest;
+import com.kd.activityService.dto.ActivityResponse;
+import com.kd.activityService.service.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/activities/")
+public class ActivityController {
+
+    @Autowired
+    private ActivityService activityService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping("/get-activity")
+    public ResponseEntity<List<ActivityResponse>> getActivities(@RequestHeader("X-User-ID") String userId){
+        return ResponseEntity.ok(activityService.getActivities(userId));
+    }
+
+    @GetMapping("/get-activity/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable("activityId") String activityId){
+        return ResponseEntity.ok(activityService.getActivitesById(activityId));
+    }
+
+}
