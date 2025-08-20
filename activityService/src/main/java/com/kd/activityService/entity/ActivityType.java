3b3f58ca3eabd600200d69079ac58d9743bb4d31
Kalana_KD <kalanakd471@gmail.com>
2025-08-20 09:13:07 +0530

    Finish ActivityType Enum file

---
+++ activityService/src/main/java/com/kd/activityService/entity/ActivityType.java
@@ -0,0 +1,14 @@
+package com.kd.activityService.entity;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_LIFTING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
