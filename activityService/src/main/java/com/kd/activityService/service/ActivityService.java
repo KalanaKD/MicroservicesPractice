42fdf5b282bce86e006019acb27dd583be1216f0
Kalana_KD <kalanakd471@gmail.com>
2025-08-21 09:00:22 +0530

    Update ActivityService

--- activityService/src/main/java/com/kd/activityService/service/ActivityService.java
+++ activityService/src/main/java/com/kd/activityService/service/ActivityService.java
@@ -18,9 +18,15 @@ public class ActivityService {
 
     @Autowired
     private final ActivityRepo activityRepo;
+    private final UserValidationService userValidationService;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
+        boolean isValidUser = userValidationService.validateUser(request.getUserId());
+
+        if (!isValidUser) {
+            throw new RuntimeException("Invalid user id" + request.getUserId()) ;
+        }
         Activity activity = Activity.builder()
                 .userId(request.getUserId())
                 .activityType(request.getActivityType())
