f099536a6b8393f3ee4770c195edf96b5b8c1c26
Kalana_KD <kalanakd471@gmail.com>
2025-08-23 06:19:40 +0530

    update ActivityServiceListner properties

--- aiservices/src/main/java/com/kd/aiservices/service/ActivityServiceListner.java
+++ aiservices/src/main/java/com/kd/aiservices/service/ActivityServiceListner.java
@@ -1,6 +1,8 @@
 package com.kd.aiservices.service;
 
 import com.kd.aiservices.model.Activity;
+import com.kd.aiservices.model.Recommendation;
+import com.kd.aiservices.repo.RecommendationRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,10 +14,13 @@ import org.springframework.stereotype.Service;
 public class ActivityServiceListner {
 
     private final ActivityAi activityAi;
+    private final RecommendationRepo recommendationRepo;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
-        log.info("Received activity for proecssing {}", activity.getId());
-        log.info("Generated Recommendation : {}", activityAi.generateRecommendation(activity));
+        log.info("Received activity for processing {}", activity.getId());
+        //log.info("Generated Recommendation : {}", activityAi.generateRecommendation(activity));
+        Recommendation recommendation = activityAi.generateRecommendation(activity);
+        recommendationRepo.save(recommendation);
     }
 }
