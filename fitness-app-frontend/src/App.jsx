6710d2c5154e44dacce5b8e75790b403e2ad09f4
Kalana_KD <kalanakd471@gmail.com>
2025-09-06 10:00:00 +0530

    update app.jsx

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { BrowserRouter as Router, Navigate , Route, Routes , useLocation } from "react-router";
+import Button from '@mui/material/Button';
 
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Button variant="contained" color="#dc004e">Login</Button>
+      </Router>
     </>
   )
 }
