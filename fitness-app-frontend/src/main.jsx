ad5f9054b42e7cb8ba5349fd1c8e349629067aa4
Kalana_KD <kalanakd471@gmail.com>
2025-09-09 17:43:34 +0530

    refactor main.jsx: wrap App component with AuthProvider for OAuth2 authentication

--- fitness-app-frontend/src/main.jsx
+++ fitness-app-frontend/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './authConfig'
 
 import App from './App'
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig} loadingComponent={<div>Loading...</div>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>
+
 )
